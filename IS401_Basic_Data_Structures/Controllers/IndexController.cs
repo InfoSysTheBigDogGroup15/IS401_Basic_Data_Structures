22a6c8c8feae75a7d2113204e0a89d2f862ae1dd
Highlander336 <connorraymcleod@gmail.com>
2018-09-29 11:24:31 -0600

    Update IndexController.cs
    I tried to do this from visual studio but it's slow...

--- IS401_Basic_Data_Structures/Controllers/IndexController.cs
+++ IS401_Basic_Data_Structures/Controllers/IndexController.cs
@@ -8,10 +8,31 @@ namespace IS401_Basic_Data_Structures.Controllers
 {
     public class IndexController : Controller
     {
+        //random name generator code
+
+        public static Random random = new Random();
+
+        public static string randomName()
+        {
+            string[] names = new string[8] { "Dan Morain", "Emily Bell", "Carol Roche", "Ann Rose", "John Miller", "Greg Anderson", "Arthur McKinney", "Joann Fisher" };
+            int randomIndex = Convert.ToInt32(random.NextDouble() * 7);
+            return names[randomIndex];
+        }
+
+        public static int randomNumberInRange()
+        {
+            return Convert.ToInt32(random.NextDouble() * 20);
+        }
+
         // GET: Index
         public ActionResult Index()
         {
+            //Create variables
+            Queue<string> lineOfCustomers = new Queue<string>();
+            Dictionary<string, int> customerInfo = new Dictionary<string, int>();
+            //Load variables
+
+
             return View();
         }
-    }
 }
