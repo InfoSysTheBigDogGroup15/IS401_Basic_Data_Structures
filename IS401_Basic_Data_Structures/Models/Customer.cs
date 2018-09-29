83ca17b359c57e75a666be3df488b575ce22f46a
Hoyong Jung <jhoyong57@gmail.com>
2018-09-29 14:16:07 -0600

    changed route

---
+++ IS401_Basic_Data_Structures/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IS401_Basic_Data_Structures.Models
+{
+    public class Customer
+    {
+    }
+}
